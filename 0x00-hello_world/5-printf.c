af3d147927b6783fb2324c71fe00464747e14adb
Desekeye <triciadede024@gmail.com>
2022-06-30 07:55:49 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0. (epitychia)
  */
-int main(void).
-{puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
