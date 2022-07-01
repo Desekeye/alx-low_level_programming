1c945b8412d395f50d3a2c7a870d92abf589d2ef
Desekeye <triciadede024@gmail.com>
2022-07-01 07:25:29 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,6 +2,7 @@
  * file: ad task 101-print_comb4.c
  * auth: PDT_Desekeye
  */
+#include <stdio.h>
 /**
  * main - entry point
  *
@@ -10,13 +11,10 @@
 int main(void)
 { int digit1, digit2, digit3;
 	for (digit1 = 0; digit1 < 8; digit1++)
-
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-
 		{
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
