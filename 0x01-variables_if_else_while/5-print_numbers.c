a79cc1e0cc9e0053224c8cab73b0e2d97b132096
Desekeye <triciadede024@gmail.com>
2022-07-01 04:47:31 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+/*
+ * file: task 5-print_numbers.c
+ * auth:PDT
+ */
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (epitychia)
+ */
+int main(void)
+{ int num;
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
