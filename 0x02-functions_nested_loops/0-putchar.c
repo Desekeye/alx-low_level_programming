3ceefce54ced30dd47cb44a73372064b6a1334ec
Desekeye <triciadede024@gmail.com>
2022-07-05 04:47:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+/*
+ * file: 0-putchar.c
+ * auth: PDT_Desekeye
+ */
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (epitychia)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putcahr('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+/*
+ * file: 1-alphabet.c
+ * auth: PDT_Desekeye
+ */
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (epitychia)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n')
+		return (0);
+}
