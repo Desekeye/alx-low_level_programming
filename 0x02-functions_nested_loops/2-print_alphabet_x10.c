992a10b7f60caf5cd35a46437a4e1a1c3c985706
Desekeye <triciadede024@gmail.com>
2022-07-05 09:05:41 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,15 @@
-/*
- * file: 2-print_alphabet_x10.c
- * auth: PDT_Desekeye
- */
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet in lowercase
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase.
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
+	int count = o;
 	char letter;
-
-	while (count++ <= 9)
-	{
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n')
-	}
+while (count++ <= 9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
 }
--- 0x02-functions_nested_loops/main.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-/**
- * 1-alphabet.c - prints alphabets
- * Return: nothing
- */
-void 1-alphabet.c(void)
-
