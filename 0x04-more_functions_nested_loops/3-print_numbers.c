084f14b7e52f2d1d893e63fcfe03ea5449b3889c
Desekeye <triciadede024@gmail.com>
2022-07-07 11:35:04 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,21 @@
 /*
  * file: 3-print_numbers.c
- * auth: PDT
+ * auth: PDT_Desekeye
  */
 
 #include "main.h"
 
 /**
  * print_numbers -  prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: Always 0 (epitychia)
  */
-
 void print_numbers(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
-
+	int num
+		for (num = 0; num <= 9; num++)
+			_putchar((num % 10) + '0');
 	_putchar('\n');
+
+	return (0);
 }
