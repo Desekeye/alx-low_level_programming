9ac353595b167785fb489723a8d40c0da7a3d428
Desekeye <triciadede024@gmail.com>
2022-07-07 09:25:12 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+/*
+ * file: 3-print_numbers.c
+ * auth: PDT
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
