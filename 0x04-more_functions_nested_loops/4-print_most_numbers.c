5b4dfd6900db67510f5cb887520664886f1dce73
Desekeye <triciadede024@gmail.com>
2022-07-07 11:49:01 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+/*
+ * file: 4-print_most_numbers.c
+ * auth: PDT_Desekeye
+ */
+
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Always 0 (epitychia)
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+			_putchar((num % 10) + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
