d83599e468b3986d7de605786dc458ca1be621de
Desekeye <triciadede024@gmail.com>
2022-07-07 15:42:41 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,15 +3,16 @@
 /**
  * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
  */
-
 void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (count = 0; count <= 9; count++)
 	{
-		if (num != 2 && num != 4)
-			_putchar((num % 10) + '0')
+		if (num == '2' || num == '4')
+			continue;
+		else
+			_putchar(num);
 	}
 	_putchar('\n');
 }
