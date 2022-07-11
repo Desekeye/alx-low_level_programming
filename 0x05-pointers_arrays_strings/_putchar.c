bdf17cc533a0a54b7c0308d1af210312dfc833a4
Desekeye <triciadede024@gmail.com>
2022-07-11 08:36:14 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,10 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: Alwayas 1. (epitychia)
+ * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
