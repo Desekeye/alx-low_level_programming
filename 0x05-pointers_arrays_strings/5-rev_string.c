f521087a02bd7d4553415f099674ecc967c2401e
Desekeye <triciadede024@gmail.com>
2022-07-11 04:52:30 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: String to be reversed
+ *
+ * Return: Always 0. (epitychia)
+ */
+
+void rev_string(char *s);
+{
+	int i, j;
+	char temp;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+
+	i--;
+
+	for (j = 0; j <= i; j++, i--)
+	{
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+	}
+
+}
