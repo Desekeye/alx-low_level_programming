00de6a5f2a9d76433603fed6af2e34766fce2abc
Desekeye <triciadede024@gmail.com>
2022-08-04 11:14:08 -0700

    its

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -86,28 +86,28 @@ void print_all(const char * const format, ...)
                 {"i", print_int},
                 {"f", print_float},
                 {"s", print_string}
-        };
-
-        va_start(args, format);
-
-        while (format && (*(format + i)))
-        {
-                j = 0;
-
-                while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-                        j++;
-
-                if (j < 4)
-                {
-                        printf("%s", separator);
-                        funcs[j].print(args);
-                        separator = ", ";
-                }
-
-                i++;
-        }
-
-        printf("\n");
-
-        va_end(args);
+       	};
+       
+	va_start(args, format);
+       
+	while (format && (*(format + i)))
+       	{
+	       	j = 0;
+	       
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+		       	j++;
+	       
+		if (j < 4)
+	       	{
+		       	printf("%s", separator);
+		       	funcs[j].print(args);
+		       	separator = ", ";
+		}
+		
+		i++;
+       	}
+       
+	printf("\n");
+       
+	va_end(args);
 }
