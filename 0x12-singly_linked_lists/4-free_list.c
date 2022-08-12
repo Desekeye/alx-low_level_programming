6ff2c100a857bab3de5cd56cb74976a020254dd4
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-08-12 09:23:37 +0200

    linked

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,22 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * free_list - Frees a list_t list.
- * @head: A pointer to the list_t list.
+ * free_list - function with one argument
+ * @head: pointer to list_t
+ *
+ * Description: frees a list
+ * Return: na
  */
 void free_list(list_t *head)
 {
-	list_t *tmp;
+	list_t *cursor;
 
 	while (head)
 	{
-		tmp = head->next;
+		cursor = head->next;
 		free(head->str);
 		free(head);
-		head = tmp;
+		head = cursor;
 	}
 }
