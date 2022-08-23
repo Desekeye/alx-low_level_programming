29cd0f65cf4ae0b7d0d6f302d1d358020f79645a
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-08-23 20:55:15 +0200

    file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,73 @@
-#include "main.h"
-
+#include "mainh"
+void closer(int arg_files);
 /**
- * main - function with two arguments
- * @argc: count of arguments
- * @argv: variadic arguments
- *
- * Description: copies the content of a file to another file
- * Return: 0 for success and exit codes for failure
+ * main - Entry Point
+ * @argc: # of args
+ * @argv: array pointer for args
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	int to = 0, from = 0, to_count = 0;
-	char buffer[1024];
-	int from_count = 1024;
+	int file_from, file_to, file_from_r, wr_err;
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO,
-			"Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	from = open(argv[1], O_RDONLY);
-	if (from == 1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (to == 1)
+	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
-	while (from_count == 1024)
+	while (file_from_r >= 1024)
 	{
-		from_count = read(from, buffer, 1024);
-		if (from_count == -1)
+		file_from_r = read(file_from, buf, 1024);
+		if (file_from_r == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			closer(file_from);
+			closer(file_to);
 			exit(98);
 		}
-		to_count = write(to, buffer, from_count);
-		if (to_count == -1)
+		wr_err = write(file_to, buf, file_from_r);
+		if (wr_err == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 
-	if (close(from)  == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
-		exit(100);
-	}
-	if (close(to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
-		exit(100);
-	}
-
+	closer(file_from);
+	closer(file_to);
 	return (0);
+}
+
+/**
+ * closer - close with error
+ * @arg_files: argv 1 or 2
+ * Return: void
+ */
+void closer(int arg_files)
+{
+	int close_err;
+
+	close_err = close(arg_files);
+
+	if (close_err == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", arg_files);
+		exit(100);
+	}
 }
