ce0ed03950478d4801ac89abdc9614a8c8405bd0
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-09-29 23:58:46 +0200

    C_again

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,30 +12,36 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new;
+	dlistint_t *new, *cursor;
 
+	if (h == NULL)
+		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (; idx != 1; idx--)
+	/* loop until nth node of idx */
+	cursor = *h;
+	while (idx > 1 && cursor && cursor->next)
 	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
+		cursor = cursor->next;
+		idx--;
 	}
-
-	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+	if (idx > 1 || cursor == NULL)
+		return (NULL);
 
+	/* assign value to new node */
 	new->n = n;
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp->next->prev = new;
-	tmp->next = new;
+
+	/* insert node */
+	if (cursor->next != NULL)
+		cursor->next->prev = new;
+	new->prev = cursor;
+	new->next = cursor->next;
+	cursor->next = new;
 
 	return (new);
 }
