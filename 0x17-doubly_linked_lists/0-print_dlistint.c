582506c43e567201e0400ab77be7b2cc9a8d7670
Desekeye <106808436+Desekeye@users.noreply.github.com>
2022-09-29 23:45:35 +0200

    C_again

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0
-		while (h)
-		{
-			node++;
-			printf("%d\n", h->n);
-			h = h->next;
-		}
-	return (nodes);
+	int count;
+
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (count);
 }
